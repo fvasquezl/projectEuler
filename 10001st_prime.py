1c19a15d72e2d2df1673d3a14d8a13bde5a4d43b
Faustino Vasquez <vasquez.limon@gmail.com>
2024-02-19 21:51:24 -0800

    num primos

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -1,16 +1,37 @@
 #!/bin/python3
 
 import sys
+import time
+import math
 
-def is_primo(n):
-    count=0
-    for i in range(n):
-       if n%i==0:
-           count+=1
-    if count==2:
-        print('es primo')
+l1=[2,3]
+n=3
+while len(l1)<100:
+    n+=2
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+           break
+        else:
+            l1.append(n)
+
 
 
 t = int(input().strip())
+
 for a0 in range(t):
     n = int(input().strip())
+
+    tiempo_inicio = time.time()
+    print(l1)
+    # i=2
+    # p=1
+    # while p< n:
+    #     i+=1
+    #     if is_prime(i):
+    #         p+=1
+        
+           
+
+    tiempo_fin = time.time()  # Registra el tiempo de finalización
+    tiempo_total = tiempo_fin - tiempo_inicio
+    print(tiempo_total)
